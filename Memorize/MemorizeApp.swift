fbcde76aabf1339938f81e44d50c384d21a27f49
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2025-05-31 13:59:15 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Shaikat on 31.5.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
